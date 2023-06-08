6aeafe0079db983dc41193461de1b05365b1f4eb
Rebeca Gasso <Rebeca_Gasso@gap.com>
2023-06-08 18:50:36 -0300

    feat: novos atributos e metodo estaVencido

--- src/classes/Loja.java
+++ src/classes/Loja.java
@@ -8,10 +8,12 @@ public class Loja {
 
     // construtores
 
-    public Loja(String nome, int quantidadeFuncionarios, float salarioBaseFuncionario){
+    public Loja(String nome, int quantidadeFuncionarios, float salarioBaseFuncionario, Endereco endereco, Data dataDeFundacao){
         this.nome = nome;
         this.quantidadeFuncionarios = quantidadeFuncionarios;
         this.salarioBaseFuncionario = salarioBaseFuncionario;
+        this.endereco = endereco;
+        this.dataDeFundacao = dataDeFundacao;
     }
     public Loja(String nome, int quantidadeFuncionarios){
         this.nome = nome;
@@ -57,7 +59,7 @@ public class Loja {
     }
 
     public String toString(){
-        return "A loja" + this.nome + "tem" + this.quantidadeFuncionarios + "com salario base de " + this.salarioBaseFuncionario;
+        return "A loja" + this.nome + "tem" + this.quantidadeFuncionarios + "com salario base de " + this.salarioBaseFuncionario + ". Foi fundada em " + this.dataDeFundacao + "e esta localizada em " + this.endereco;
     }
 
     public float gastosComSalario(){
--- src/classes/Produto.java
+++ src/classes/Produto.java
@@ -2,10 +2,12 @@ package classes;
 public class Produto {
     String nome;
     double preco;
+    Data dataDeValidade;
 
-    public Produto(String nome, double preco){
+    public Produto(String nome, double preco, Data dataDeValidade){
         this.nome = nome;
         this.preco = preco;
+        this.dataDeValidade = dataDeValidade;
     };
 
     public String getNome() {
@@ -22,7 +24,24 @@ public class Produto {
         this.preco = preco;
     };
 
+    public Data getDataDeValidade() {
+        return dataDeValidade;
+    }
+    public void setDataDeValidade(Data dataDeValidade) {
+        this.dataDeValidade = dataDeValidade;
+    }
+
     public String toString(){
-        return "O Produto" + this.nome + "possui o preço" + this.preco;
+        return "O Produto" + this.nome + "possui o preço" + this.preco + "e data de validade " + this.dataDeValidade;
     };
+
+    public boolean estaVencido(Data data){
+        if(data.ano > this.dataDeValidade.ano){
+            return true;
+        } else if(data.ano == this.dataDeValidade.ano && data.mes >= this.dataDeValidade.mes && data.dia >= this.dataDeValidade.dia){
+            return true;
+        } else{
+            return false;
+        }
+    }
 };
